1767c6085cccfc3f9488703d73c96e30773ed765
khb4435 <79688282+khb4435@users.noreply.github.com>
2023-12-11 00:04:08 +0900

    Delete C directory

--- C/LinearDataStructure/06_DetachFiles/test.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-void InitDummyData(void);
-void TestStep01(void);
-void TestStep02(void);
--- C/LinearDataStructure/06_DetachFiles/ui.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-typedef enum MY_MENU { EXIT, NEW, SEARCH, PRINT, REMOVE } MY_MENU;
-MY_MENU PrintMenu(void);
-void EventLoopRun(void);
-void PrintList(void);
-void PrintListReverse(void);
-void Search(void);
